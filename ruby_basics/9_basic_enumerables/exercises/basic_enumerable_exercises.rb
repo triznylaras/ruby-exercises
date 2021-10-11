d650fd3b327e978077dac4196a624a1d005fb174
triznylaras <triznylaras@gmail.com>
2021-10-11 15:43:05 +0700

    complete nested array & basic enumerables exercises

--- ruby_basics/9_basic_enumerables/exercises/basic_enumerable_exercises.rb
+++ ruby_basics/9_basic_enumerables/exercises/basic_enumerable_exercises.rb
@@ -32,5 +32,5 @@ def find_word_lengths(word_list)
   # use #reduce to iterate through each item of the word_list (an array)
   # return a hash with each word as the key and its length as the value
   # hint: look at the documentation and review the reduce examples in basic enumerable lesson
-  word_list.reduce { |word| word_hash[word] = word.length }
+  word_list.reduce { |word| word_hash[word] = word.length}
 end
